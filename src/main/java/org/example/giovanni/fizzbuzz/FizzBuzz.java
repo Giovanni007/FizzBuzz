b005275d34ceb7e65e66e90fc28934e0ee6b447d
Giovanni <giovanni@Giovannis-MacBook-Pro.local>
2020-02-19 22:55:34 +0000

    Step2

--- src/main/java/org/example/giovanni/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/giovanni/fizzbuzz/FizzBuzz.java
@@ -2,33 +2,53 @@ package org.example.giovanni.fizzbuzz;
 
 
 public class FizzBuzz {
-	private final String MULTIPLE_OF_3_STR = "fizz";
-	private final String MULTIPLE_OF_5_STR = "buzz";
-	private final String MULTIPLE_OF_15_STR = "fizzbuzz";
+	static final String MULTIPLE_OF_3_STR = "fizz";
+	static final String MULTIPLE_OF_5_STR = "buzz";
+	static final String MULTIPLE_OF_15_STR = "fizzbuzz";
+	static final String CONTAINS_3_STR = "alfresco";
 
 
 	public String getFizzBuzzString(int x, int y) {
 		
+		//Check range 
 		if(x<=0 || y <= 0) throw new IllegalArgumentException();
-		
 		int start = Integer.min(x, y);
 		int end = Integer.max(x, y);
+		
 		StringBuilder sb = new StringBuilder();
-	
+		
+		//Create fizzbuzz string 
 		for(int i=start; i<=end; i++) {
-			if(isMultiple(i,15)) sb.append(MULTIPLE_OF_15_STR);
-			else if(isMultiple(i,5)) sb.append(MULTIPLE_OF_5_STR);
-			else if(isMultiple(i,3)) sb.append(MULTIPLE_OF_3_STR);
-			else sb.append(i);
+			sb.append(getFizzBuzzWord(i));
 			sb.append(" ");
 		}
 		
+		//Delete the last space
 		sb.setLength(sb.length()-1);
 		return sb.toString();
 	}
 	
 	private boolean isMultiple(int input, int number) {
 		return input%number == 0;
+	}
+	
+	public String getFizzBuzzWord(int num) {
+		
+		if(containsDigit(num,3)) return CONTAINS_3_STR;
+		
+		if(isMultiple(num,15)) return MULTIPLE_OF_15_STR;
+		if(isMultiple(num,5)) return MULTIPLE_OF_5_STR;
+		if(isMultiple(num,3)) return MULTIPLE_OF_3_STR;
+		return String.valueOf(num);
+	}
+	
+	private boolean containsDigit(int num, int digit) {
+		int currentNumber = num;
+		while (currentNumber > 0 ) {
+		    if (currentNumber % 10 == digit) return true;
+		    currentNumber = currentNumber / 10;
+		}
+		return false;
 	}
 	
 }
--- src/test/java/org/example/giovanni/fizzbuzz/FIzzBuzzTest.java
+++ src/test/java/org/example/giovanni/fizzbuzz/FIzzBuzzTest.java
@@ -22,9 +22,21 @@ public class FIzzBuzzTest {
 
 	@ParameterizedTest
 	@CsvSource({ "1,1,1", "1,2,1 2",
-			"1,20,1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz" })
+			"1,20,1 2 alfresco 4 buzz fizz 7 8 fizz buzz 11 fizz alfresco 14 fizzbuzz 16 17 fizz 19 buzz"
+	})
 	void getFizzBuzzString_validRange_returnfizzBuzzString(int x, int y, String expected) {
 		assertEquals(fizzBuzz.getFizzBuzzString(x, y), expected);
+	}
+	
+	@ParameterizedTest
+	@CsvSource({ "1,1", 
+				"3," + FizzBuzz.CONTAINS_3_STR,
+				"15," + FizzBuzz.MULTIPLE_OF_15_STR,
+				"5," + FizzBuzz.MULTIPLE_OF_5_STR,
+				"9," + FizzBuzz.MULTIPLE_OF_3_STR
+		})
+	void getFizzBuzzWord_validinput_returnfizzBuzzWord(int x,String expected) {
+		assertEquals(fizzBuzz.getFizzBuzzWord(x), expected);
 	}
 
 }
