7fbbf6a87dae4091a316de3adf5129996acfcc4f
Giovanni <giovanni@Giovannis-MacBook-Pro.local>
2020-02-20 21:27:03 +0000

    Step3

--- src/main/java/org/example/giovanni/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/giovanni/fizzbuzz/FizzBuzz.java
@@ -1,14 +1,27 @@
 package org.example.giovanni.fizzbuzz;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class FizzBuzz {
+	public static final Logger logger = LoggerFactory.getLogger(FizzBuzz.class);
+
 	static final String MULTIPLE_OF_3_STR = "fizz";
 	static final String MULTIPLE_OF_5_STR = "buzz";
 	static final String MULTIPLE_OF_15_STR = "fizzbuzz";
 	static final String CONTAINS_3_STR = "alfresco";
+	static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
 
 	public String getFizzBuzzString(int x, int y) {
+		Map<String,Integer> reportMap = initializeReportMap();
 		
 		//Check range 
 		if(x<=0 || y <= 0) throw new IllegalArgumentException();
@@ -19,12 +32,20 @@ public class FizzBuzz {
 		
 		//Create fizzbuzz string 
 		for(int i=start; i<=end; i++) {
-			sb.append(getFizzBuzzWord(i));
+			String fizzBuzzWord = getFizzBuzzWord(i);
+			sb.append(fizzBuzzWord);
 			sb.append(" ");
+			
+			addToReport(fizzBuzzWord,reportMap);
 		}
 		
 		//Delete the last space
 		sb.setLength(sb.length()-1);
+		
+		sb.append(LINE_SEPARATOR);
+		//Add report at the end of the fizzBuzz string
+		sb.append(getReportString(reportMap));
+		logger.debug(sb.toString());
 		return sb.toString();
 	}
 	
@@ -49,6 +70,31 @@ public class FizzBuzz {
 		    currentNumber = currentNumber / 10;
 		}
 		return false;
+	}
+	
+	private Map<String,Integer> initializeReportMap() {
+		Map<String,Integer> map = new LinkedHashMap<> ();
+		map.put(MULTIPLE_OF_3_STR,0);
+		map.put(MULTIPLE_OF_5_STR,0);
+		map.put(MULTIPLE_OF_15_STR,0);
+		map.put(CONTAINS_3_STR,0);
+		map.put("integer",0);
+		
+		return map;
+
+	}
+	
+	private void addToReport(String word, Map<String,Integer> map) {
+        BiFunction<String, Integer, Integer> add = (key, val) -> val + 1; 
+
+		if(map.computeIfPresent(word, add) == null)
+			map.compute("integer", add);
+	}
+	
+	String getReportString(Map<String,Integer> map) {
+	    return map.entrySet().stream()
+	        .map(e -> e.getKey() + ": " + e.getValue())
+	        .collect(Collectors.joining(" "));
 	}
 	
 }
--- src/test/java/org/example/giovanni/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/example/giovanni/fizzbuzz/FizzBuzzTest.java
@@ -3,13 +3,17 @@ package org.example.giovanni.fizzbuzz;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class FIzzBuzzTest {
+public class FizzBuzzTest {
 	private final FizzBuzz fizzBuzz = new FizzBuzz();
 
 	@ParameterizedTest
@@ -21,11 +25,23 @@ public class FIzzBuzzTest {
 	}
 
 	@ParameterizedTest
-	@CsvSource({ "1,1,1", "1,2,1 2",
-			"1,20,1 2 alfresco 4 buzz fizz 7 8 fizz buzz 11 fizz alfresco 14 fizzbuzz 16 17 fizz 19 buzz"
-	})
+	@MethodSource("provideInputforGetFizzBuzzString_validRange")
 	void getFizzBuzzString_validRange_returnfizzBuzzString(int x, int y, String expected) {
 		assertEquals(fizzBuzz.getFizzBuzzString(x, y), expected);
+	}
+	
+	private static Stream<Arguments> provideInputforGetFizzBuzzString_validRange() {
+	    return Stream.of(
+	      Arguments.of(1, 1,"1" +
+	    		  FizzBuzz.LINE_SEPARATOR + 
+	    		  "fizz: 0 buzz: 0 fizzbuzz: 0 alfresco: 0 integer: 1"),
+	      Arguments.of(1, 2,"1 2" +
+	    		   FizzBuzz.LINE_SEPARATOR + 
+	    		  "fizz: 0 buzz: 0 fizzbuzz: 0 alfresco: 0 integer: 2"),
+	      Arguments.of(1, 20,"1 2 alfresco 4 buzz fizz 7 8 fizz buzz 11 fizz alfresco " +
+	    		  "14 fizzbuzz 16 17 fizz 19 buzz" 
+	    		  + FizzBuzz.LINE_SEPARATOR + 
+	    		  "fizz: 4 buzz: 3 fizzbuzz: 1 alfresco: 2 integer: 10"));
 	}
 	
 	@ParameterizedTest
